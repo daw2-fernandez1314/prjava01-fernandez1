f54255971f8b49656b1cc5e74754125548e42c22
ubu64 <ubu64@ubu64DVL-Fernandez>
2014-06-04 18:27:22 +0200

    version 0.1 del projecto prjava01-fernandez

---
+++ src/prjava01/fernandez/Prjava01Fernandez.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01.fernandez;
+
+/**
+ *
+ * @author ubu64
+ */
+
+import java.awt.Desktop;
+import java.io.*;
+
+public class Prjava01Fernandez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Nova p&agrave;gina web de Daniel Fernandez");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    Nova p&agrave;gina web de Daniel Fernandez");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+}
---
+++ src/prjava01/fernandez/bw.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prjava01.fernandez;
+
+/**
+ *
+ * @author ubu64
+ */
+class bw {
+    
+}
